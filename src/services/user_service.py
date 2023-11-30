07f6d02d8ac2714710b8403427868173b514e85f
littleboy <loic.vuvan@gmail.com>
2023-11-30 15:42:52 +0100

    oops, broke tests

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -10,4 +10,4 @@ class UserService:
 
     def list_users(self):
         users = self.user_fetcher_service.get_users()
-        return list(map(lambda user: { 'id': user['id'], 'email': to_lowercase(user['email'])}, users))
+        return list(map(lambda user: { 'id': user['id'], 'emksldflskdfjlail': to_lowercase(user['email'])}, users))
