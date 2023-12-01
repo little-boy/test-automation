85a70f0e72c661b22a3073d6bd9e8fa7aaafb914
littleboy <loic.vuvan@gmail.com>
2023-12-01 10:45:14 +0100

    test enforcing no direct push on main

--- tests/test_string_service.py
+++ tests/test_string_service.py
@@ -6,6 +6,7 @@ from src.services.string_service import to_uppercase
 def test_uppercase_with_lowercased_str():
     assert to_uppercase('hello r2-d2') == 'HELLO R2-D2'
 
+
 def test_uppercase_with_uppercased_str():
     assert to_uppercase('HELLO YOU R2D2') == 'HELLO YOU R2D2'
 
